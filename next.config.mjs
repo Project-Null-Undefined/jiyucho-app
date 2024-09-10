fda1535c049ef293f5b0608ee071c56b48cc1c95
satooru65536 <ryuto4619@gmail.com>
2024-09-10 18:14:51 +0900

    chore: scss周り設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    additionalData: `
+      @use '@/styles/colors.scss' as *;
+      @use '@/styles/variables.scss' as *;    
+      @use '@/styles/mixins.scss' as *;
+    `,
+  },
+};
 
 export default nextConfig;
