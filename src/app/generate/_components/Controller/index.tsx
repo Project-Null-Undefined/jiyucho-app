1db5f254026c6e4071ff29d76268a2e2f76d1144
satooru65536 <ryuto4619@gmail.com>
2024-09-11 15:20:27 +0900

    feat: コントローラ

--- src/app/generate/_components/Controller/index.tsx
+++ src/app/generate/_components/Controller/index.tsx
@@ -1,9 +1,56 @@
+"use client";
+
+import IonIcon from "@reacticons/ionicons";
 import styles from "./index.module.scss";
+import IconButton from "@/components/share/IconButton";
 
 export default function Controller() {
   return (
     <section className={styles.controller}>
-      <p>コントローラー</p>
+      <div className={styles.group}>
+        <IconButton
+          onClick={() => console.log("clicked")}
+          icon={<IonIcon name="return-up-back-outline" size="large" />}
+        />
+        <IconButton
+          onClick={() => console.log("clicked")}
+          icon={<IonIcon name="return-down-forward-outline" size="large" />}
+        />
+      </div>
+
+      <div className={styles.group}>
+        <IconButton
+          onClick={() => console.log("clicked")}
+          icon={<IonIcon name="play-back-outline" size="large" />}
+        />
+        <IconButton
+          onClick={() => console.log("clicked")}
+          icon={<IonIcon name="play-skip-back-outline" size="large" />}
+        />
+        <IconButton
+          onClick={() => console.log("clicked")}
+          icon={<IonIcon name="play-outline" size="large" />}
+        />
+        <IconButton
+          onClick={() => console.log("clicked")}
+          icon={<IonIcon name="play-skip-forward-outline" size="large" />}
+        />
+        <IconButton
+          onClick={() => console.log("clicked")}
+          icon={<IonIcon name="play-forward-outline" size="large" />}
+        />
+      </div>
+
+      <div className={styles.group}>
+        <IconButton
+          onClick={() => console.log("clicked")}
+          icon={<IonIcon name="save-outline" size="large" />}
+        />
+        <IconButton
+          onClick={() => console.log("clicked")}
+          icon={<IonIcon name="share-outline" size="large" />}
+        />
+      </div>
     </section>
   );
 }
