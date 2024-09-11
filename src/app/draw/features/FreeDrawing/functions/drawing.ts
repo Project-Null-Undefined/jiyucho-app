a37ac9537f56612ea4caacae317e9aee8387a420
karasuneo <yusei4520030401@gmail.com>
2024-09-11 18:10:39 +0900

    add: 描写制御用の関数を追加

---
+++ src/app/draw/features/FreeDrawing/functions/drawing.ts
@@ -0,0 +1,29 @@
+import paper from "paper";
+
+
+export function setupDrawingTool() {
+  let path: paper.Path | null = null;
+
+  const tool = new paper.Tool();
+
+  tool.onMouseDown = (event: paper.ToolEvent) => {
+    path = new paper.Path();
+    path.strokeColor = new paper.Color("#4597f8");
+    path.strokeWidth = 2;
+    path.add(event.point);
+  };
+
+  tool.onMouseDrag = (event: paper.ToolEvent) => {
+    if (path) {
+      path.add(event.point);
+    }
+  };
+
+  tool.onMouseUp = () => {
+    if (path) {
+      path.smooth();
+    }
+  };
+
+  return tool;
+}
