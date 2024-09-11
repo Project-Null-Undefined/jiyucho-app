31e02229f612dc8fb68462fc63d60dc51f50f5ae
satooru65536 <ryuto4619@gmail.com>
2024-09-11 16:26:49 +0900

    feat: 鍵盤

--- src/app/generate/_components/Piano/index.tsx
+++ src/app/generate/_components/Piano/index.tsx
@@ -1,9 +1,23 @@
 import styles from "./index.module.scss";
+import PianoKey from "./PianoKey";
+
+const MAX_OCTAVE = 4;
+const MIN_OCTAVE = 2;
+export const WHITE_KEYS = ["C", "D", "E", "F", "G", "A", "B"] as const;
 
 export default function Piano() {
+  const length = MAX_OCTAVE - MIN_OCTAVE + 1;
+  const octaves = Array.from({ length }, (_, i) => MAX_OCTAVE - i);
+
   return (
     <section className={styles.piano}>
-      <div>ピアノ</div>
+      {octaves.map((octave) =>
+        [...WHITE_KEYS]
+          .reverse()
+          .map((scale, i) => (
+            <PianoKey key={i} scale={scale} octave={octave} />
+          )),
+      )}
     </section>
   );
 }
