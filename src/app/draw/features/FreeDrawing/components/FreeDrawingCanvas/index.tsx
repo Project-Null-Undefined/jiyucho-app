0a3941b469a1d88a852b4fbbc9fcc5a22c843109
karasuneo <yusei4520030401@gmail.com>
2024-09-11 18:27:31 +0900

    add: 自由帳のコンポーネントを定義

---
+++ src/app/draw/features/FreeDrawing/components/FreeDrawingCanvas/index.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from "react";
+import { useDrawing } from "../../hooks/useDrawing";
+import styles from "./styles.module.scss";
+
+function FreeDrawingCanvas(): ReactElement {
+  const { canvasRef } = useDrawing();
+
+  return (
+    <canvas
+      ref={canvasRef}
+      className={styles.canvas}
+      
+    />
+  );
+}
+
+export default FreeDrawingCanvas;
