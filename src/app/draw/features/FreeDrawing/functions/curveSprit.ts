59b823b71bb09f88a2f56b9e3aa486f20dbe4672
karasuneo <yusei4520030401@gmail.com>
2024-09-12 18:37:10 +0900

    add: 曲線分割のロジックを追加

---
+++ src/app/draw/features/FreeDrawing/functions/curveSprit.ts
@@ -0,0 +1,12 @@
+import { BAR_NUM, NOTE_NUM } from "../const";
+import { Coordinate } from "../dto/curve";
+
+export function splitCurve(coordinates: Coordinate[]): Coordinate[] {
+  const resultCurve = Array.from(
+    { length: BAR_NUM * NOTE_NUM },
+    (_, i) =>
+      coordinates[Math.floor((i * coordinates.length) / (BAR_NUM * NOTE_NUM))]
+  );
+
+  return resultCurve;
+}
---
+++ src/app/draw/features/FreeDrawing/functions/intervalCreate.ts
@@ -0,0 +1,30 @@
+import { Interval, ScaleInterval } from "@/tyeps";
+import { Coordinate } from "../dto/curve";
+import { HALF_NOTE, SCALE_NUM, WHOLE_NOTE } from "../const";
+
+export function createInterval(curveCoordinates: Coordinate[]): Interval[] {
+  const yBase = curveCoordinates[0].y;
+  const yMax = Math.max(...curveCoordinates.map((c) => c.y - yBase));
+  const yMin = Math.min(...curveCoordinates.map((c) => c.y - yBase));
+  const yMaxAbs = Math.max(Math.abs(yMax), Math.abs(yMin));
+
+  const normalizedCurve = curveCoordinates.map((c) => ({
+    x: c.x,
+    y: Math.abs((c.y - yBase) / yMaxAbs),
+  }));
+  const scaledCurveY: ScaleInterval[] = normalizedCurve.map((nc) => {
+    return Math.floor(nc.y * SCALE_NUM) as ScaleInterval;
+  });
+
+  const interval: Interval[] = scaledCurveY.map((scy) => {
+    if (WHOLE_NOTE.includes(scy)) {
+      return 2;
+    } else if (HALF_NOTE.includes(scy)) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  return interval;
+}
