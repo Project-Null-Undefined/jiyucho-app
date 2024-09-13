2cf71b9a8ccb9433ec2ee3d19741300410c159c1
satooru65536 <ryuto4619@gmail.com>
2024-09-13 18:47:45 +0900

    feat: コード選択欄追加

--- src/app/generate/_components/Blocks/index.tsx
+++ src/app/generate/_components/Blocks/index.tsx
@@ -4,51 +4,9 @@ import { useAtomValue } from 'jotai';
 import styles from './index.module.scss';
 import { barCountAtom, octaveRangeAtom, beatCountAtom, minNoteDurationAtom } from '@/stores/settings';
 import { CSSProperties, useMemo } from 'react';
-import { Bar, DiatonicChord, Music, Note } from '@/models';
 import Block from './Block';
 import { MAX, MIN } from '@/const';
-
-// 仮の楽譜
-const music = new Music({
-  bars: [
-    new Bar({
-      notes: [
-        new Note({ scale: 'C', octave: 4, start: 0, duration: 4 }),
-        new Note({ scale: 'D', octave: 4, start: 4, duration: 4 }),
-      ],
-      chord: new DiatonicChord({
-        scale: 'C',
-        octave: 2,
-        start: 0,
-        duration: 16,
-      }),
-    }),
-    new Bar({
-      notes: [
-        new Note({ scale: 'C', octave: 5, start: 0, duration: 4 }),
-        new Note({ scale: 'D', octave: 5, start: 4, duration: 4 }),
-      ],
-      chord: new DiatonicChord({
-        scale: 'C',
-        octave: 2,
-        start: 0,
-        duration: 16,
-      }),
-    }),
-    new Bar({
-      notes: [
-        new Note({ scale: 'C', octave: 3, start: 0, duration: 4 }),
-        new Note({ scale: 'D', octave: 3, start: 4, duration: 4 }),
-      ],
-      chord: new DiatonicChord({
-        scale: 'C',
-        octave: 2,
-        start: 0,
-        duration: 16,
-      }),
-    }),
-  ],
-});
+import { music } from '@/samples';
 
 export default function Blocks() {
   const octaveRange = useAtomValue(octaveRangeAtom); // 表示する音域
--- src/app/generate/_components/CodeSelector/index.tsx
+++ src/app/generate/_components/CodeSelector/index.tsx
@@ -1,5 +1,39 @@
+'use client';
+
+import { music } from '@/samples';
 import styles from './index.module.scss';
+import { useAtomValue } from 'jotai';
+import { barCountAtom, beatCountAtom, minNoteDurationAtom } from '@/stores/settings';
+import { CSSProperties, useMemo } from 'react';
+import Select from './Select';
 
 export default function CodeSelector() {
-  return <section className={styles.code_selector}>コード進行選択</section>;
+  const barCount = useAtomValue(barCountAtom); // 小節数
+  const beatCount = useAtomValue(beatCountAtom); // 拍子
+  const minNoteDuration = useAtomValue(minNoteDurationAtom); // 最小音符の長さ
+
+  const style = useMemo(
+    () =>
+      ({
+        // 1小節あたりのカラム数
+        ['--cols-per-bar' as string]: beatCount * minNoteDuration,
+        ['--bar-count' as string]: barCount,
+      }) satisfies CSSProperties,
+    [barCount, beatCount, minNoteDuration],
+  );
+
+  return (
+    <section className={styles.code_selector_section} style={style}>
+      {Array.from({ length: barCount }).map((_, i) => {
+        const bar = music.bars.at(i);
+        if (bar) return <Select key={bar.id} bar={bar} />;
+
+        return (
+          <span key={i} className={styles.none}>
+            -
+          </span>
+        );
+      })}
+    </section>
+  );
 }
