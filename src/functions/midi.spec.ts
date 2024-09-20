bb41d23d8f11a35c00e8db6d9ceaa7c57e5e0a67
k21091 <k21091kk@aitech.ac.jp>
2024-09-20 23:45:23 +0900

    fix:一部関数がテスト環境で使えなかったのでモック化

--- src/functions/midi.spec.ts
+++ src/functions/midi.spec.ts
@@ -1,8 +1,14 @@
-import { describe, test, expect } from 'vitest';
+import { describe, test, expect, beforeAll, vi } from 'vitest';
 import { createMidiFile, save } from './midi';
 import { music } from '@/samples';
 
 describe('MIDI', () => {
+  // midi.spec.ts のテストファイルで追加
+  beforeAll(() => {
+    global.URL.createObjectURL = vi.fn(() => 'mocked-url');
+     // URL.revokeObjectURLのモック
+    global.URL.revokeObjectURL = vi.fn();
+  });
   test('exportMidi', () => {
     const midiContent = createMidiFile(music, 1);
 
