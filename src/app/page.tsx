54d716520b4dec74c0ef9d6351c5a3b1275c20a2
satooru65536 <ryuto4619@gmail.com>
2024-09-20 20:15:47 +0900

    change: スタイルを調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import styles from './page.module.scss';
+
 export default function MainPage() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <img alt="logo" src="/images/logo.svg" />
+        <a href="/draw">start</a>
+      </div>
+    </main>
   );
 }
